5d1f38911d1995f0a3a081b97cee5f3730903596
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-09-13 23:48:00 +0300

    Create animals table and Insert data

--- schema.sql
+++ schema.sql
@@ -1,5 +1,10 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE animals (
-    name varchar(100)
+CREATE TABLE public.animals (
+    animal_id INT GENERATED ALWAYS AS IDENTITY,
+    animal_name VARCHAR,
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg decimal
 );
