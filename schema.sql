ce98435f544c9ec533a3554c1c0ffdd88f654af7
alanluqman <35001742+alanluqman@users.noreply.github.com>
2022-09-14 12:16:18 +0300

    Update Query.sql

--- schema.sql
+++ schema.sql
@@ -1,10 +1,11 @@
 /* Database schema to keep the structure of entire database. */
 
-CREATE TABLE public.animals (
+CREATE TABLE animals (
     animal_id INT GENERATED ALWAYS AS IDENTITY,
     animal_name VARCHAR,
     date_of_birth date,
-    escape_attempts integer,
+    escape_attempts INT,
     neutered boolean,
-    weight_kg decimal
+    weight_kg decimal,
+    PRIMARY KEY (animal_id)
 );
